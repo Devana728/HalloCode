fa1edaf56d994723537b0893802c8283f4a9ce9b
devana <devana728@rambler.ru>
2022-08-04 04:33:31 +0400

    Examples005 IfElse

---
+++ Examples005 IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("введите ваше имя");
+string username = Console.ReadLine();
+if(username = маша)
+{
+   Console.WriteLine("Ура,это же Маша!");
+}
+else
+{
+   Console.WriteLine("Привет, ");   
+   Console.WriteLine(username);
+}
